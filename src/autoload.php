18a54908a4dfb4acfd95ab2889973b8da7b23a19
Rémi Darricau <remi@copromatic.com>
2016-10-15 22:53:57 +0200

    Autoload

--- src/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-spl_autoload_register(function ($class) {
-    require("Creneau/autoload.php");
-    require("PointRelais/autoload.php");
-    require("Quickcost/autoload.php");
-    require("Shipping/autoload.php");
-    require("Tracking/autoload.php");
-});
